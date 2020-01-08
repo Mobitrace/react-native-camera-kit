0d3a8d5e2164b284ae3389356b595f40090da1a7
michielkn-mobitrace <35763788+michielkn-mobitrace@users.noreply.github.com>
2020-01-08 13:12:45 +0100

    Update CameraKitGallery.android.js

--- src/CameraKitGallery.android.js
+++ src/CameraKitGallery.android.js
@@ -3,52 +3,48 @@ const NativeGalleryModule = NativeModules.NativeGalleryModule;
 import _ from 'lodash';
 
 async function getAlbumsWithThumbnails() {
-  const albums = await NativeGalleryModule.getAlbumsWithThumbnails();
-  return albums;
+    const albums = await NativeGalleryModule.getAlbumsWithThumbnails();
+    return albums;
 }
 
 async function getImageUriForId(imageId) {
-  // Return what getImagesForIds() typically returns in the 'uri' field.
-  return `file://${imageId}`;
+    // Return what getImagesForIds() typically returns in the 'uri' field.
+    return `file://${imageId}`;
 }
 
 async function getImagesForIds(imagesUris = []) {
-  return await NativeGalleryModule.getImagesForUris(imagesUris);
+    return await NativeGalleryModule.getImagesForUris(imagesUris);
 }
 
 async function getImageForTapEvent(nativeEvent) {
-  const selectedImageId = nativeEvent.selected;
-  const imageUri = selectedImageId && await getImageUriForId(selectedImageId);
-  return {selectedImageId, imageUri, width: nativeEvent.width, height: nativeEvent.height};
+    const selectedImageId = nativeEvent.selected;
+    const imageUri = selectedImageId && await getImageUriForId(selectedImageId);
+    return {selectedImageId, imageUri, width: nativeEvent.width, height: nativeEvent.height};
 }
 
 async function getImagesForCameraEvent(event) {
-  if (!event.captureImages) {
-    return [];
-  }
-  
-  const images = [];
-  event.captureImages.forEach(async (image) => {
-    images.push({
-      ...image,
-      uri: await getImageUriForId(image.uri)
+    if (!event.captureImages) {
+        return [];
+    }
+
+    const images = [];
+    event.captureImages.forEach(async (image) => {
+        images.push({
+            ...image,
+            uri: await getImageUriForId(image.uri)
+        });
     });
-  });
-  return images;
-}
-async function resizeImage(image = {}, quality = 'original') {
-  console.log('Warning - CameraKitGallery.resizeImage isn\'t  support on Android yet');
-  return;
+    return images;
 }
 
 async function checkDevicePhotosAuthorizationStatus() {
-  const isAuthorized = await NativeGalleryModule.checkDeviceStorageAuthorizationStatus();
-  return isAuthorized;
+    const isAuthorized = await NativeGalleryModule.checkDeviceStorageAuthorizationStatus();
+    return isAuthorized;
 }
 
 async function requestDevicePhotosAuthorization() {
-  const isAuthorized = await NativeGalleryModule.requestDeviceStorageAuthorization();
-  return isAuthorized;
+    const isAuthorized = await NativeGalleryModule.requestDeviceStorageAuthorization();
+    return isAuthorized;
 }
 
 async function resizeImage(image = {}, quality = 'original') {
@@ -61,12 +57,12 @@ async function resizeImage(image = {}, quality = 'original') {
 
 
 export default {
-  checkDevicePhotosAuthorizationStatus,
-  requestDevicePhotosAuthorization,
-  getAlbumsWithThumbnails,
-  getImageUriForId,
-  getImagesForIds,
-  getImageForTapEvent,
-  getImagesForCameraEvent,
-  resizeImage
+    checkDevicePhotosAuthorizationStatus,
+    requestDevicePhotosAuthorization,
+    getAlbumsWithThumbnails,
+    getImageUriForId,
+    getImagesForIds,
+    getImageForTapEvent,
+    getImagesForCameraEvent,
+    resizeImage
 }
